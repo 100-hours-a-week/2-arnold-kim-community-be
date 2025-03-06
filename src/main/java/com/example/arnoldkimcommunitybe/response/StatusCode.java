c07c4312b40cbf5d2bf5bc22594998271c5b9d39
tpgh02 <seaho4588@gmail.com>
2025-03-06 17:51:58 +0900

    [feat] response template 작성

---
+++ src/main/java/com/example/arnoldkimcommunitybe/response/StatusCode.java
@@ -0,0 +1,21 @@
+package com.example.arnoldkimcommunitybe.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum StatusCode {
+    OK(HttpStatus.OK, "OK"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Bad Request"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "Not Found"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+
+    private final HttpStatus status;
+    private final String message;
+
+    public String getMessage(Throwable throwable) {
+        return throwable.getMessage();
+    }
+}
