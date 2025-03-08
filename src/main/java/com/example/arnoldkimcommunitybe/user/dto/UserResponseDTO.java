9fbd5fa7441b10aa984729db4d9bd245c033475d
tpgh02 <seaho4588@gmail.com>
2025-03-08 13:28:48 +0900

    [feat] 사용자 정보 가져오는 기능 구현

--- src/main/java/com/example/arnoldkimcommunitybe/user/dto/UserResponseDTO.java
+++ src/main/java/com/example/arnoldkimcommunitybe/user/dto/UserResponseDTO.java
@@ -6,5 +6,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UserResponseDTO {
-
+    private String email;
+    private String username;
+    private String profile;
 }
