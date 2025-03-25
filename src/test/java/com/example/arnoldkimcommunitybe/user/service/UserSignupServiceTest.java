3ddc468ad65f4ad919ac1a5a75f928b22cd89e10
tpgh02 <seaho4588@gmail.com>
2025-03-25 11:43:52 +0900

    [test] 회원가입 실패 테스트

--- src/test/java/com/example/arnoldkimcommunitybe/user/service/UserSignupServiceTest.java
+++ src/test/java/com/example/arnoldkimcommunitybe/user/service/UserSignupServiceTest.java
@@ -5,8 +5,12 @@ import com.example.arnoldkimcommunitybe.user.UserEntity;
 import com.example.arnoldkimcommunitybe.user.UserRepository;
 import com.example.arnoldkimcommunitybe.user.UserService;
 import com.example.arnoldkimcommunitybe.user.dto.UserRequestDTO;
+import org.apache.coyote.BadRequestException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
@@ -14,6 +18,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -26,20 +31,34 @@ class UserSignupServiceTest {
     @Autowired
     private UserService userService;
 
-    @DisplayName("회원가입 - 실패")
-    @Test
-    void createUserFail() {
-        // given
-        String email = "test@test.com";
-        String password = "Test1234!";
-        String passwordCheck = "Test1234!";
-        String username = "test";
-        MultipartFile image = new MockMultipartFile("image",
-                "test.jpg",
-                "image/jpeg",
-                "dummy image content".getBytes());
+    private static Stream<Arguments> invalidSignup() {
+        return Stream.of(
+                Arguments.of(true, true, "test@test.com", "Test1234!", "Test1234!", "sayho", new MockMultipartFile("image",
+                        "test.jpg",
+                        "image/jpeg",
+                        "dummy image content".getBytes())),
+                Arguments.of(false, true, "test@test.com", "Test1234!", "Test1234!", "sayho", new MockMultipartFile("image",
+                        "test.jpg",
+                        "image/jpeg",
+                        "dummy image content".getBytes())),
+                Arguments.of(true, false, "test@test.com", "Test1234!", "Test1234!", "sayho", new MockMultipartFile("image",
+                        "test.jpg",
+                        "image/jpeg",
+                        "dummy image content".getBytes())),
+                Arguments.of(false, false, "test@test.com", "Test1234!", "Test12345!", "sayho", new MockMultipartFile("image",
+                        "test.jpg",
+                        "image/jpeg",
+                        "dummy image content".getBytes())),
+                Arguments.of(false, false, "test@test.com", "Test1234!", "Test1234!", "sayho", null)
+        );
+    }
 
-        UserRequestDTO userRequestDTO = UserRequestDTO.builder()
+    @DisplayName("회원가입 - 실패")
+    @ParameterizedTest
+    @MethodSource("invalidSignup")
+    void createUserFail(boolean isEmailExist, boolean isUsernameExists, String email, String password, String passwordCheck, String username, MockMultipartFile image) {
+        // given
+        UserRequestDTO request = UserRequestDTO.builder()
                 .email(email)
                 .password(password)
                 .passwordCheck(passwordCheck)
@@ -47,11 +66,16 @@ class UserSignupServiceTest {
                 .build();
 
         // mocking
-        when(userRepository.existsByEmail(userRequestDTO.getEmail())).thenReturn(true);
-        when(userRepository.existsByUsername(userRequestDTO.getUsername())).thenReturn(true);
+        when(userRepository.existsByEmail(request.getEmail())).thenReturn(isEmailExist);
+        when(userRepository.existsByUsername(request.getUsername())).thenReturn(isUsernameExists);
 
         // when then
-        assertThrows(ConfilctException.class, () -> userService.createUser(userRequestDTO, image));
+        if (image != null) {
+            assertThrows(ConfilctException.class, () -> userService.createUser(request, image));
+        } else {
+            assertThrows(BadRequestException.class, () -> userService.createUser(request, image));
+        }
+
     }
 
     @DisplayName("회원가입 - 성공")
